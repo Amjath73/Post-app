f13e0132c8c4aa8073c232321aea7b79d62974cc
Amjath73 <amjathsanker4@gmail.com>
2024-06-07 20:18:49 +0530

    Search and deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPost from './components/AddPost';
+import SearchPost from './components/SearchPost';
+import DeletePost from './components/DeletePost';
 
 function App() {
   return (
     <div>
       <AddPost/>
+      <SearchPost/>
+      <DeletePost/>
     </div>
   );
 }
