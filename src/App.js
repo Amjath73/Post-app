81b1ffea51dc32a2f3111cc6361046834f5ea03a
Amjath73 <amjathsanker4@gmail.com>
2024-06-07 19:28:32 +0530

    added post

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddPost from './components/AddPost';
+
+function App() {
+  return (
+    <div>
+      <AddPost/>
+    </div>
+  );
+}
+
+export default App;
